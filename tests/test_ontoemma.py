07d0385a5b4352c351eab682432707344266bad7
Lucy Lu Wang <lucyw@lucyw.local>
2017-11-10 12:38:52 -0800

    remove nn model from tests folder; this will be trained and generated each time the test is run

--- tests/test_ontoemma.py
+++ tests/test_ontoemma.py
@@ -1,12 +1,44 @@
 from emma.OntoEmma import OntoEmma
 import os
+import shutil
 import unittest
 
 TEST_DATA = os.path.join('tests', 'data')
 
 
 class TestOntoEmmaAlign(unittest.TestCase):
-    def test_align_lr(self):
+
+    def test_nn(self):
+        config_file = os.path.join(TEST_DATA, 'test_nn_config_file.json')
+        model_path = os.path.join(TEST_DATA, 'test_nn_model')
+
+        if os.path.exists(model_path):
+            shutil.rmtree(model_path)
+
+        matcher = OntoEmma()
+        matcher.train(
+            'nn', model_path, config_file
+        )
+
+        assert(os.path.exists(os.path.join(model_path, 'model.tar.gz')))
+
+        model_path = os.path.join(TEST_DATA, 'test_nn_model', 'model.tar.gz')
+        source_ont_file = os.path.join(TEST_DATA, 'test_source_ont.json')
+        target_ont_file = os.path.join(TEST_DATA, 'test_target_ont.json')
+        input_alignment_file = os.path.join(TEST_DATA, 'test_input_alignment.tsv')
+        output_alignment_file = os.path.join(TEST_DATA, 'test_output_alignment.tsv')
+
+        matcher = OntoEmma()
+        p, r, f1 = matcher.align(
+            'nn', model_path,
+            source_ont_file, target_ont_file,
+            input_alignment_file, output_alignment_file
+        )
+        assert p >= 0.0
+        assert r >= 0.0
+        assert f1 >= 0.0
+
+    def test_lr(self):
         model_path = os.path.join(TEST_DATA, 'test_lr_model.pickle')
         source_ont_file = os.path.join(TEST_DATA, 'test_source_ont.json')
         target_ont_file = os.path.join(TEST_DATA, 'test_target_ont.json')
@@ -24,19 +56,3 @@ class TestOntoEmmaAlign(unittest.TestCase):
         assert r >= 0.6
         assert f1 >= 0.7
 
-    def test_train_nn(self):
-        model_path = os.path.join(TEST_DATA, 'test_nn_model', 'model.tar.gz')
-        source_ont_file = os.path.join(TEST_DATA, 'test_source_ont.json')
-        target_ont_file = os.path.join(TEST_DATA, 'test_target_ont.json')
-        input_alignment_file = os.path.join(TEST_DATA, 'test_input_alignment.tsv')
-        output_alignment_file = os.path.join(TEST_DATA, 'test_output_alignment.tsv')
-
-        matcher = OntoEmma()
-        p, r, f1 = matcher.align(
-            'nn', model_path,
-            source_ont_file, target_ont_file,
-            input_alignment_file, output_alignment_file
-        )
-        assert p >= 0.0
-        assert r >= 0.0
-        assert f1 >= 0.0
